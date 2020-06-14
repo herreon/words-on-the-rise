5a98674cc375e8eaecc46b996af2b925a38a9d26
HyoL <58346289+herreon@users.noreply.github.com>
2020-06-14 13:17:12 -0700

    Update search terms

--- src/generator.js
+++ src/generator.js
@@ -12,64 +12,23 @@ function arrayDataGenerator () {
     geo: "US"
   })
   .then(termResult => {
-    // console.log(termResult)
+    console.log("res", termResult)
     // console.log(JSON.parse(termResult).default.timelineData)
-    let output = JSON.parse(termResult).default.timelineData;
-    return JSON.stringify(output, null, 2);
+    // let output = JSON.parse(termResult).default.timelineData;
+    // console.log(JSON.stringify(output, null, 2));
+    // return JSON.stringify(output, null, 2);
   })
   
-  .then(test => {
-    fs.writeFile(__dirname + '/../dist/assets/data.json', test, function (err) {
-      if (err) { console.log(err); }
-      else { console.log('write operation complete.') }
-    })
-  })
+  // .then(test => {
+  //   fs.writeFile(__dirname + '/../dist/assets/data.json', test, function (err) {
+  //     if (err) { console.log(err); }
+  //     else { console.log('write operation complete.') }
+  //   })
+  // })
   
   .catch(err => console.log("there is an error!", err))
 }
 
-function dataGenerator () {
 
-  searchTerms.forEach(term => {
-
-    console.log("term", term)
-
-    googleTrends.interestOverTime({
-      keyword: term,
-      startTime: new Date(2017, 11, 1),
-      geo: "US"
-    })
-
-    .then(termResult => {
-
-      // console.log("termResult", termResult)
-      
-      let termArray = JSON.parse(termResult).default.timelineData;
-
-      allTerms[term] = termArray;
-
-      return JSON.stringify(allTerms, null, 2);
-
-      // resultsArray.forEach(res => {
-        // console.log(res)
-        // searchPoints[res.formattedAxisTime] = res.value[0];
-      // });
-
-      // let output = JSON.stringify(searchPoints, null, 2)
-      
-    })
-
-    .then(test => {
-      fs.writeFile(__dirname + '/../dist/assets/data.json', test, function (err) {
-        if (err) { console.log(err); }
-        else { console.log('write operation complete.') }
-      })
-    })
-
-    .catch(err => console.log("oh no error!", err))
-  })
-
-}
-
+console.log(searchTerms);
 arrayDataGenerator();
-// dataGenerator();
--- src/searchTerms.js
+++ src/searchTerms.js
@@ -1,26 +1,101 @@
-const searchTerms = [
-    "what does vsco girl mean",
-    "what does ok boomer mean",
-    "what does uwu mean",
-    "what does simp mean"
-]
+// const $2019 = [
+//     'boomer',
+//     'vsco',
+//     'simp',
+//     'sfs',
+//     'fyp',
+//     'ngl',
+//     's/u',
+//     'iykyk',
+//     'sb',
+//     'sksksk',
+//     'yktv',
+//     'and i oop'
+// ]
 
-// ["what does ok boomer mean", "ok boomer beaning", "what does boomer mean", "boomer meaning"];
-
-const waiting = [
-    "sfs",
-    "ngl",
-    "simp"
-]
-
-const sorted = [
-    "asmr",
-    "simp",
+const $2019 = [
     "boomer",
-    "vsco girl",
-    "and I oop",
+    "vsco",
+    "simp",
     "sfs",
-    "ngl"
+    "fyp",
+    "ngl",
+    "s/u",
+    "iykyk",
+    "sb",
+    "sksksk",
+    "yktv",
+    "and i oop"
+]
+
+const oneHit = ["slatt", "ants"];
+
+let searchTerms = [];
+
+$2019.forEach(term => {
+    // searchTerms.push("what does " + term + " mean")
+    searchTerms.push(`what does ${term} mean`)
+})
+
+
+const $2018 = [
+    "incel",
+    "uwu",
+    "mukbang",
+    "ibf",
+    "blockchain",
+    "rng",
+    "jit",
+    "juuling",
+    "gekyume",
+    "zaddy",
+    "wtm",
+    "wig"
+]
+
+
+const $2017 = [
+    "non binary",
+    "bitcoin",
+    "snowflake",
+    "pmoys",
+    "maga",
+    "wasp",
+    "boujie",
+    "trolls"
+]
+
+const $2016 = [
+    "gaslighting",
+    "sjw",
+    "boolin",
+    "woke",
+    "shill",
+    "quash",
+    "brandish",
+    "triggered",
+    "shook",
+    "bigly",
+    "holosexual",
+    "vexing",
+    "bough",
+    "juju",
+    "wavy"
+]
+
+const $2015 = [
+    "simmer",
+    "netflix and chill",
+    "chapo",
+    "stoop",
+    "blase",
+    "pleb",
+    "lowkey",
+    "tome",
+    "wap",
+    "shudder",
+    "burnt",
+    "vape"
 ]
 
 module.exports = searchTerms;
